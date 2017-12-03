fd1bdc45f3affe02c397ed4aee67b85b4a89b8ad
Pawel Wlazlo <mrogur@gmail.com>
2017-12-03 18:37:27 +0100

    installed dependencies

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+const config = {
+    entry: {
+        app: './assets/js/app.js',
+    },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {importLoaders: 1}
+                        },
+                        'postcss-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader']
+                })
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('/css/[name].css'),
+        new BrowserSyncPlugin({
+            proxy: 'musial-kancelaria.dev',
+            port: 3000,
+            files: [
+                '**/*.php'
+            ],
+            ghostMode: {
+                clicks: false,
+                location: false,
+                forms: false,
+                scroll: false
+            },
+            injectChanges: true,
+            logFileChanges: true,
+            logLevel: 'debug',
+            logPrefix: 'wepback',
+            notify: true,
+            reloadDelay: 0
+        })
+    ]
+};
+
+//If true JS and CSS files will be minified
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new UglifyJSPlugin(),
+        new OptimizeCssAssetsPlugin()
+    );
+}
+
+module.exports = config;
